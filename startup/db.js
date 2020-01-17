300bbcb6745371c04f7d95cbb20dd3e3ecc3ad4a
Vikash151098 <vkumar0961@gmail.com>
2020-01-17 10:56:45 +0530

    add support for connStr

--- startup/db.js
+++ startup/db.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 const config = require('config');
 
 module.exports = function() {
-  const db = config.get('db');
-  mongoose.connect(db,{
-    useNewUrlParser:true })
+  const db = 'mongodb+srv://vidlyuser:12345@cluster0-onnex.mongodb.net/test?retryWrites=true&w=majority';
+
+  mongoose.connect(db)
     .then(() => winston.info(`Connected to ${db}...`));
 }
