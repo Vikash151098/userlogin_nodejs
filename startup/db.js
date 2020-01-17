2e71e7a84d0b5fc319e08399f17ee3adf1f0e54a
Vikash151098 <vkumar0961@gmail.com>
2020-01-17 11:11:54 +0530

    add connStr

--- startup/db.js
+++ startup/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const config = require('config');
 
 module.exports = function() {
-  const db = 'mongodb+srv://vidlyuser:12345@cluster0-onnex.mongodb.net/test?retryWrites=true&w=majority';
+  const db = 'mongodb://vidlyuser:<password>@cluster0-shard-00-00-onnex.mongodb.net:27017,cluster0-shard-00-01-onnex.mongodb.net:27017,cluster0-shard-00-02-onnex.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority';
 
   mongoose.connect(db)
     .then(() => winston.info(`Connected to ${db}...`));
